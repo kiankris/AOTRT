f061980da5a71d6d46dc51c11693c0709470af81
Kiankris Villagonzalo <kcv@utexas.edu>
2017-08-17 00:47:38 -0500

    Can now enter in numbers and a word to produce applicable combinations for the word

--- src/app/images/images.component.ts
+++ src/app/images/images.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-images',
@@ -8,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class ImagesComponent implements OnInit {
 
   imageURLs: any[] = [];
+  @Input() letters: string[] = ["", "", "", ""];
+  map: string[];
+  answer: string[];
+  finalValues: any[] = [[], [], [], []];
+  @Input() word: string;
 
 
   constructor() { }
@@ -15,13 +20,85 @@ export class ImagesComponent implements OnInit {
   ngOnInit() {
     let s;
     let l = 0;
-
-    for (let i = 0; i < 26; i++) {
-      s = String.fromCharCode(l++ + 65);
-      this.imageURLs.push("assets/" + s + ".jpg");
+    let r1, r2;
+    for (let k = 0; k < 2; k++) {
+      r1 = [];
+      for (let i = 0; i < 7; i++) {
+        s = String.fromCharCode(l++ + 65);
+        r1.push("assets/" + s + ".jpg");
+      }
+      r2 = [];
+      for (let i = 0; i < 6; i++) {
+        s = String.fromCharCode(l++ + 65);
+        r2.push("assets/" + s + ".jpg");
+      }
+      this.imageURLs.push(r1);
+      this.imageURLs.push(r2);
     }
-    console.log(JSON.stringify(this.imageURLs));
+    console.log("size: " + this.imageURLs.length);
     console.log("Finished initializing images array")
   }
 
+  eval(position, c) {
+    let arr = this.finalValues[position - 1];
+    let letter = this.letters[position - 1];
+    if (letter == null || letter.length == 0) {
+      return
+    }
+    while (arr.length > 0) {
+      arr.pop()
+    }
+    for (let i = 0; i < 4; i++) {
+      arr.push(Number(letter) + (c * i))
+    }
+
+    this.map = this.determineFinalValues();
+    this.populateAnswer();
+
+  }
+
+  determineFinalValues() {
+    let answers = [];
+    for (let i = 0; i < 26; i++) {
+      answers.push(-1);
+    }
+
+    for (let row of this.finalValues) {
+      if (row != null && row.length > 0) {
+        answers[row[0] - 1] = String(row[0]);
+        this.helpers(answers, 1, row[0], String(row[0]));
+      }
+    }
+    return answers;
+  }
+
+  helpers(arr, col, val, str) {
+    if (col < 4) {
+      for (let row of this.finalValues) {
+        if (row.length > 0) {
+          let newVal = (val + row[col] - 1) % 26;
+          let newStr = str + " + " + row[col];
+          if (newVal) {
+            if (typeof arr[newVal] === "number" || arr[newVal].length > newStr.length) {
+              arr[newVal] = newStr;
+              console.log(newStr, "=", val + row[col]);
+            }
+          }
+          this.helpers(arr, col + 1, val + row[col], newStr);
+        }
+      }
+    }
+  }
+
+  populateAnswer() {
+    if (this.word != null || this.word.length > 0) {
+      let word = this.word.toUpperCase();
+      this.answer = [];
+      if (this.map != null) {
+        for (let c of word) {
+          this.answer.push(c + " : " + this.map[c.charCodeAt(0) - 65]);
+        }
+      }
+    }
+  }
 }
